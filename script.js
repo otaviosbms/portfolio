e5745b2bc16b982e3e24e2c424b029d788af022f
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-01-19 23:50:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ var btnescuro = document.getElementById('btnescuro');
 
 var body = document.querySelector('body');
 
-var topbox = document.querySelector('header');
+var topbox = document.getElementById('topbox');
 var rightbox = document.getElementsByClassName('rightboxcontainer');
 var infobox = document.getElementsByClassName('infoboxcontainer');
 var nicho = document.getElementsByClassName('nicho');
 
 btnclaro.onclick = function claro(){
     body.className = "modoclaroback";
-    topbox.className = "modoclarotopbox"
-    infobox.className = "cusujo";
+    topbox.className = "modoclarotopbox";
+    rightbox
 }
 
 btnescuro.onclick = function escuro(){
     body.className = "";
-    topbox.className = "modoescurotopbox"
+    topbox.className = "topboxcontainer";
+
 }
